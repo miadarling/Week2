f81cf693448e7dab5ddf8b2192931b3f670e29b8
Mia Darling Ibáñez Risso <32085275+miadarling@users.noreply.github.com>
2017-10-10 00:24:34 -0400

    Create data-types-W2.py

---
+++ data-types-W2.py
@@ -0,0 +1,4 @@
+dresser = 5 #number of drawers on my dresser
+areWet = True #state of dishes in the sink
+namesofbooks = ["21stcenturyart", "howtowritecomedies", "mrsdalloway", "theuniversehasyourback"] #names of books on my bookshelf
+grocerylist = ["juice", "milk", "bagels", "cheese"]
