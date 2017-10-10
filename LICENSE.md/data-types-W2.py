ba119ad8f242f0576c53c101105ef10d0426fe05
Mia Darling Ibáñez Risso <32085275+miadarling@users.noreply.github.com>
2017-10-10 00:23:59 -0400

    Delete data-types-W2.py

--- LICENSE.md/data-types-W2.py
+++
@@ -1,4 +0,0 @@
-dresser = 5 #number of drawers on my dresser
-areWet = True #state of dishes in the sink
-namesofbooks = ["21stcenturyart", "howtowritecomedies", "mrsdalloway", "theuniversehasyourback"] #names of books on my bookshelf
-grocerylist = ["juice", "milk", "bagels", "cheese"]
